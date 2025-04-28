b8ea0e6ff215f8269695c0078abc6b41aabf4742
nullqube <nullqube@yahoo.com>
2025-04-28 16:22:37 +0330

    cli.js begining

---
+++ bin/cli.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+// Use default import for CommonJS modules
+import { Command } from 'commander';
+const program = new Command();
+
+import gulp from 'gulp';
+
+// Import your existing Gulp tasks
+import '../src/index.mjs'; // This loads your current Gulpfile
+
+program
+    .command('build')
+    .description('Build the project')
+    .action(() => {
+        console.log('Building project...');
+        gulp.series('build')();
+    });
+
+program
+    .command('serve')
+    .description('Start the development server')
+    .action(() => {
+        console.log('Starting development server...');
+        gulp.series('default')();
+    });
+
+program.parse(process.argv);
