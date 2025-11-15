2bf95ca43e53bd2cb102609e26d83f285e7145f3
nullqube <nullqube@yahoo.com>
2025-11-15 16:42:31 +0330

    Add version arg to the cli

--- bin/cli.js
+++ bin/cli.js
@@ -2,13 +2,29 @@
 
 // Use default import for CommonJS modules
 import { Command } from 'commander';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import fs from 'fs';
+import * as tasks from '../src/index.mjs'; // This loads your current Gulpfile
+
 const program = new Command();
 
-// import gulp from 'gulp';
+const INITIALIZE = 0x1;
+const COMMON = 0x2;
+const INITCONFIG = 0x4;
+// then we can have a single main function with config 
+// in binary format to do them as necessary. like in c++
 
-// Import your existing Gulp tasks
-import * as tasks from '../src/index.mjs'; // This loads your current Gulpfile
-import { watch } from 'browser-sync';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const packageJsonPath = join(__dirname, '..', '..', 'package.json');
+const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
+
+// Add version information
+program
+    .version(packageJson.version, '-v, --version', 'Output the current version')
+    .description('Your app description');
 
 program
     .command('init')
