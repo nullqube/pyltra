b2effc3e3e76d8d19ab06622164c8d01dc0be68e
nullqube <nullqube@yahoo.com>
2025-11-19 08:13:57 +0330

    update cli to accept more template options

--- bin/cli.js
+++ bin/cli.js
@@ -15,23 +15,28 @@ const INITCONFIG = 0x4;
 // then we can have a single main function with config 
 // in binary format to do them as necessary. like in c++
 
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-const packageJsonPath = join(__dirname, '..', '..', 'package.json');
-const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
 
-// Add version information
-program
-    .version(packageJson.version, '-v, --version', 'Output the current version')
-    .description('Your app description');
+
+try {
+    const packageJsonPath = join(__dirname, '..', 'package.json');
+    const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
+    
+    // Add version information
+    program
+        .version(packageJson.version, '-v, --version', 'Output the current version')
+        .description('Your app description');
+} catch (error) {
+    console.error('Error reading package.json:', error.message);
+    process.exit(1);
+}
 
 program
     .command('init')
+    .option('-t, --template <template>', 'Template to use', 'basic') // Default to 'basic'
     .description('Initialize the project')
     .action(() => {
-        console.log('Initializing project...');
-        // gulp.series('init')();
         tasks.initialize();
     });
 
@@ -39,8 +44,6 @@ program
     .command('build')
     .description('Build the project')
     .action(() => {
-        console.log('Building project...');
-        // gulp.series('build')();
         tasks.build();
     });
 
@@ -48,7 +51,6 @@ program
     .command('serve')
     .description('Start the development server')
     .action(() => {
-        console.log('Starting development server...');
         tasks.watch();
     });
 
