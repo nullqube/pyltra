4a409e88ee43b91b46e80194d0abe218d87ace66
nullqube <nullqube@yahoo.com>
2026-02-21 16:29:11 +0330

    update serve

--- src/tasks/serve.mjs
+++ src/tasks/serve.mjs
@@ -1,27 +1,44 @@
 import gulp from 'gulp';
-// import browserSync from 'browser-sync';
 import { PATHS, browserSyncInstance, Languages } from '../utils/config.mjs';
 
+import { htmlTask } from './html.mjs';
+import { assetsTask } from './assets.mjs';
+import { scssTask } from './scss.mjs';
+
 export const startServer = (done) => {
-    const defaultLang = Languages().length > 0 ? Languages()[0] : 'en';
-    const defaultPath = `/${defaultLang}/`;
-    console.log(`Serving ${languages} from dist/`);
-    browserSyncInstance.init({
-        server: { baseDir: PATHS.dist },
-        open: true,
-        notify: true,
-        startPath: defaultPath,
-    }, () => console.log('BrowserSync serving from dist/'));
-    done();
+  const langs = Languages();
+  const defaultLang = langs.length > 0 ? langs[0] : 'en';
+  const defaultPath = `/${defaultLang}/`;
+
+  console.log(`Serving ${langs.join(', ')} from dist/`);
+
+  browserSyncInstance.init(
+    {
+      server: { baseDir: PATHS.dist },
+      open: true,
+      notify: true,
+      startPath: defaultPath,
+    },
+    () => console.log('BrowserSync serving from dist/')
+  );
+
+  done();
 };
 
-export const watch = () => {
-    gulp.watch('src/templates/**/*.html', gulp.series('html'));
-    gulp.watch('src/data/**/*.yaml', gulp.series('html'));
-    gulp.watch('src/config.yaml', gulp.series('html'));
-    gulp.watch(PATHS.assets.css, gulp.series('assets'));
-    gulp.watch(PATHS.assets.js, gulp.series('assets'));
-    gulp.watch(PATHS.assets.img, gulp.series('assets'));
-    gulp.watch(PATHS.assets.other, gulp.series('assets'));
-    gulp.watch(PATHS.assets.scss, gulp.series('scss'));
-}
+export const watchFiles = () => {
+  gulp.watch('src/templates/**/*.html', htmlTask);
+  gulp.watch('src/data/**/*.yaml', htmlTask);
+  gulp.watch('src/config.yaml', htmlTask);
+
+  gulp.watch(PATHS.assets.scss, scssTask);
+
+  gulp.watch(
+    [
+      PATHS.assets.css,
+      PATHS.assets.js,
+      PATHS.assets.img,
+      PATHS.assets.other
+    ],
+    assetsTask
+  );
+};
