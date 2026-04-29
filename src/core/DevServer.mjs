cac49711b2faf21dd1cc1703d277403a66be276a
nullqube <nullqube@yahoo.com>
2026-04-29 17:52:23 +0330

    oop: DevServer

--- src/core/DevServer.mjs
+++ src/core/DevServer.mjs
@@ -1,18 +1,50 @@
-// 
-// 
-// 
+/**
+ * DevServer
+ * ---------
+ * Handles development server and file watchers.
+ *
+ * Wraps BrowserSync or any other server implementation.
+ */
+
+import browserSync from 'browser-sync';
+import chokidar from 'chokidar';
+import path from 'path';
 
 export class DevServer {
-    constructor( project, buildManager ) {
+
+    constructor(project, buildManager) {
         this.project = project;
         this.buildManager = buildManager;
+        this.server = browserSync.create();
     }
 
+    /**
+     * Starts dev server and watchers
+     */
     async start() {
+
         await this.buildManager.build();
-        this.startBrowserSync();
-        this.watchFiles();
+
+        const distPath = path.join(this.project.cwd, this.project.paths.dist);
+
+        this.server.init({
+            server: distPath,
+            open: true
+        });
+
+        this.watch();
     }
 
-    stop() { }
+    /**
+     * Watches src directory and rebuilds on change
+     */
+    watch() {
+        const srcPath = path.join(this.project.cwd, this.project.paths.src);
+
+        chokidar.watch(srcPath).on('change', async () => {
+            console.log('File changed. Rebuilding...');
+            await this.buildManager.build();
+            this.server.reload();
+        });
+    }
 }
